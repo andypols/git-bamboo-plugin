acc4e814486e9daa054e5bc299657ef66f53728f
Andy Pols <andy@pols.co.uk>
2009-03-21 12:50:56 +0000

    checks out the repository and pulls Now need to workout how to tell bamboo the change set

--- src/main/java/uk/co/pols/bamboo/gitplugin/GitRepository.java
+++ src/main/java/uk/co/pols/bamboo/gitplugin/GitRepository.java
@@ -1,26 +1,35 @@
 package uk.co.pols.bamboo.gitplugin;
 
 import com.atlassian.bamboo.commit.CommitFile;
+import com.atlassian.bamboo.commit.Commit;
 import com.atlassian.bamboo.repository.*;
 import com.atlassian.bamboo.utils.error.ErrorCollection;
 import com.atlassian.bamboo.v2.build.BuildChanges;
 import com.atlassian.bamboo.v2.build.BuildContext;
+import com.atlassian.bamboo.v2.build.BuildChangesImpl;
 import com.atlassian.bamboo.v2.build.repository.RepositoryEventAware;
 import com.atlassian.bamboo.ww2.actions.build.admin.create.BuildConfiguration;
 import org.apache.commons.configuration.HierarchicalConfiguration;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.tools.ant.taskdefs.Execute;
 import org.apache.tools.ant.taskdefs.PumpStreamHandler;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.ArrayList;
 
 /**
  * Provides GIT and GITHUB support for the Bamboo Build Server
+ * <p/>
+ * TODO Let user define the location of the git exe
  */
 public class GitRepository extends AbstractRepository implements SelectableAuthenticationRepository, WebRepositoryEnabledRepository, InitialBuildAwareRepository, MutableQuietPeriodAwareRepository, RepositoryEventAware {
+    private static final Log log = LogFactory.getLog(GitRepository.class);
+
     public static final String NAME = "Git";
     public static final String KEY = "git";
 
@@ -30,6 +39,9 @@ public class GitRepository extends AbstractRepository implements SelectableAuthe
 
     private String repositoryUrl;
     private String branch;
+
+    private static final String GIT_HOME = "/opt/local/bin";
+    private static final String GIT_EXE = GIT_HOME + "/git";
 
     public String getName() {
         return NAME;
@@ -95,6 +107,16 @@ public class GitRepository extends AbstractRepository implements SelectableAuthe
     public void onInitialBuild(BuildContext buildContext) {
     }
 
+    public String retrieveSourceCode(String planKey, String vcsRevisionKey) throws RepositoryException {
+        try {
+            pullFromRepository(getCheckoutDirectory(planKey), repositoryUrl);
+        } catch (IOException e) {
+            throw new RepositoryException("Failed to retrieveSourceCode", e);
+        }
+        return detectCommitsForUrl(repositoryUrl, vcsRevisionKey, new ArrayList<Commit>(), planKey);
+    }
+
+
     public boolean hasWebBasedRepositoryAccess() {
         return false;
     }
@@ -144,19 +166,24 @@ public class GitRepository extends AbstractRepository implements SelectableAuthe
     }
 
     public void preRetrieveSourceCode(BuildContext buildContext) {
-        throw new UnsupportedOperationException("TO DO");
+        // Nothing interesting to do pre source code checkout
     }
 
     public void postRetrieveSourceCode(BuildContext buildContext) {
-        throw new UnsupportedOperationException("TO DO");
+        // Nothing interesting to do after source code checkout
     }
 
-    public BuildChanges collectChangesSinceLastBuild(String string, String string1) throws RepositoryException {
-        throw new UnsupportedOperationException("TO DO");
-    }
+    public synchronized BuildChanges collectChangesSinceLastBuild(String planKey, String lastVcsRevisionKey) throws RepositoryException {
+        try {
+            String repositoryUrl = getRepositoryUrl();
+            pullFromRepository(getCheckoutDirectory(planKey), repositoryUrl);
 
-    public String retrieveSourceCode(String string, String string1) throws RepositoryException {
-        throw new UnsupportedOperationException("TO DO");
+            final List<Commit> commits = new ArrayList<Commit>();
+            String lastRevisionChecked = detectCommitsForUrl(repositoryUrl, lastVcsRevisionKey, commits, planKey);
+            return new BuildChangesImpl(String.valueOf(lastRevisionChecked), commits);
+        } catch (IOException e) {
+            throw new RepositoryException("Failed to collectChangesSinceLastBuild", e);
+        }
     }
 
     private void validateMandatoryField(BuildConfiguration buildConfiguration, ErrorCollection errorCollection, String fieldKey, String errorMessage) {
@@ -175,27 +202,87 @@ public class GitRepository extends AbstractRepository implements SelectableAuthe
         return true;
     }
 
+    private File getCheckoutDirectory(String planKey) throws RepositoryException {
+        return new File(getSourceCodeDirectory(planKey), "checkout");
+    }
+
+    private String detectCommitsForUrl(String repositoryUrl, final String lastRevisionChecked, final List<Commit> commits, String planKey) {
+//        log.error("detecting commits for "+lastRevisionChecked);
+//        GitLog gitLog = new GitLog();
+//        GitLogOptions opt = new GitLogOptions();
+//        if (lastRevisionChecked != null)
+//        {
+//            opt.setOptLimitCommitAfter(true, lastRevisionChecked);
+//        }
+//        List<GitLogResponse.Commit> gitCommits = gitLog.log(getCheckoutDirectory(planKey), opt, Ref.createBranchRef("origin/master"));
+//        if (gitCommits.size() > 1)
+//        {
+//            gitCommits.remove(gitCommits.size()-1);
+//            log.error("commits found:"+gitCommits.size());
+//            String startRevision = gitCommits.get(gitCommits.size() - 1).getDateString();
+//            String latestRevisionOnServer = gitCommits.get(0).getDateString();
+//            log.info("Collecting changes for '" + planKey + "' on path '" + repositoryUrl + "' from version " + startRevision + " to " + latestRevisionOnServer);
+//
+//            for (GitLogResponse.Commit logEntry : gitCommits)
+//            {
+//                CommitImpl commit = new CommitImpl();
+//                String authorName = logEntry.getAuthor();
+//
+//                // it is possible to have commits with empty committer. BAM-2945
+//                if (StringUtils.isBlank(authorName))
+//                {
+//                    log.info("Author name is empty for " + commit.toString());
+//                    authorName = Author.UNKNOWN_AUTHOR;
+//                }
+//                commit.setAuthor(new AuthorImpl(authorName));
+//                commit.setDate(new Date(logEntry.getDateString()));
+//                commit.setComment(logEntry.getMessage());
+//                List<CommitFile> files = new ArrayList();
+//
+//                if (logEntry.getFiles() != null) {
+//                    for (GitLogResponse.CommitFile file : logEntry.getFiles())
+//                    {
+//                        CommitFileImpl commitFile = new CommitFileImpl();
+//                        commitFile.setName(file.getName());
+//                        commitFile.setRevision(logEntry.getSha());
+//                        files.add(commitFile);
+//                    }
+//                }
+//                commit.setFiles(files);
+//
+//                commits.add(commit);
+//            }
+//            return latestRevisionOnServer;
+//        }
+        log.info("******  returning last revision:" + lastRevisionChecked);
+        return lastRevisionChecked;
+    }
+
+
     /*
     cd working directory
-    git init
+    git init -- iff it does not already exist
     git remote add origin git@github.com:andypols/polsbusiness.git
     git pull origin master
     */
+    private void pullFromRepository(File sourceDir, String repositoryUrl) throws IOException {
+        Execute execute = new Execute(new PumpStreamHandler(System.out));
+        execute.setWorkingDirectory(sourceDir);
 
-    /**
-     * Need
-     * - working directory
-     * - path to git client binary
-     * - github url
-     * - branch to pull from repository
-     *
-     * @param args
-     * @throws IOException
-     */
+        if (!sourceDir.exists()) {
+            sourceDir.mkdirs();
+            execute.setCommandline(new String[]{GIT_EXE, "init"});
+            execute.execute();
+
+            execute.setCommandline(new String[]{GIT_EXE, "remote", "add", "origin", repositoryUrl});
+            execute.execute();
+        }
+
+        execute.setCommandline(new String[]{GIT_EXE, "pull", "origin", "master"});
+        execute.execute();
+    }
+
     public static void main(String[] args) throws IOException {
-        String gitHome = "/opt/local/bin";
-        String gitExe = gitHome + "/git";
-
         File workingDirectory = new File("/Users/andy/projects/git/temp/newrepo");
 
         Execute execute = new Execute(new PumpStreamHandler(System.out));
@@ -203,14 +290,14 @@ public class GitRepository extends AbstractRepository implements SelectableAuthe
 
         if (!workingDirectory.exists()) {
             workingDirectory.mkdirs();
-            execute.setCommandline(new String[]{gitExe, "init"});
+            execute.setCommandline(new String[]{GIT_EXE, "init"});
             execute.execute();
 
-            execute.setCommandline(new String[]{gitExe, "remote", "add", "origin", "git@github.com:andypols/git-bamboo-plugin.git"});
+            execute.setCommandline(new String[]{GIT_EXE, "remote", "add", "origin", "git@github.com:andypols/git-bamboo-plugin.git"});
             execute.execute();
         }
 
-        execute.setCommandline(new String[]{gitExe, "pull", "origin", "master"});
+        execute.setCommandline(new String[]{GIT_EXE, "pull", "origin", "master"});
         execute.execute();
     }
 }
