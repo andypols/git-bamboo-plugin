62759587dc5a02dbdf68a4f1947c2019cddc7659
Andy Pols <andy@pols.co.uk>
2009-03-31 01:28:56 +0100

    more edge case tests

---
+++ src/main/java/uk/co/pols/bamboo/gitplugin/commands/ExtractorGitLogCommand.java
@@ -0,0 +1,48 @@
+package uk.co.pols.bamboo.gitplugin.commands;
+
+import com.atlassian.bamboo.commit.Commit;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class ExtractorGitLogCommand implements GitLogCommand {
+    private static final Log log = LogFactory.getLog(ExtractorGitLogCommand.class);
+
+    private String gitExe;
+    private File sourceCodeDirectory;
+    private String lastRevisionChecked;
+    private CommandExecutor commandExecutor;
+
+    public ExtractorGitLogCommand(String gitExe, File sourceCodeDirectory, String lastRevisionChecked, CommandExecutor commandExecutor) {
+        this.gitExe = gitExe;
+        this.sourceCodeDirectory = sourceCodeDirectory;
+        this.lastRevisionChecked = lastRevisionChecked;
+        this.commandExecutor = commandExecutor;
+    }
+
+    public List<Commit> extractCommits() throws IOException {
+        String logText = commandExecutor.execute(getCommandLine(), sourceCodeDirectory);
+
+        log.info(logText);
+        GitLogParser logParser = new GitLogParser(logText);
+
+        List<Commit> commits = logParser.extractCommits();
+        lastRevisionChecked = logParser.getMostRecentCommitDate();
+
+        return commits;
+    }
+
+    public String getLastRevisionChecked() {
+        return lastRevisionChecked;
+    }
+
+    private String[] getCommandLine() {
+        if (lastRevisionChecked != null) {
+            return new String[]{gitExe, "log", "--date=iso8601", "--since=\"" + lastRevisionChecked + "\""};
+        }
+        return new String[]{gitExe, "log", "-1", "--date=iso8601"};
+    }
+}
--- src/test/java/uk/co/pols/bamboo/gitplugin/GitClientTest.java
+++ src/test/java/uk/co/pols/bamboo/gitplugin/GitClientTest.java
@@ -8,6 +8,7 @@ import java.io.File;
 import java.io.IOException;
 
 import com.atlassian.bamboo.commit.Commit;
+import com.atlassian.bamboo.commit.CommitImpl;
 import com.atlassian.bamboo.build.logger.BuildLogger;
 import com.atlassian.bamboo.repository.RepositoryException;
 import uk.co.pols.bamboo.gitplugin.commands.GitPullCommand;
@@ -22,6 +23,8 @@ public class GitClientTest extends MockObjectTestCase {
     private GitPullCommand gitPullCommand = mock(GitPullCommand.class);
     private GitLogCommand gitLogCommand = mock(GitLogCommand.class);
     private static final String NO_PREVIOUS_LATEST_UPDATE_TIME = null;
+    private ArrayList<Commit> commits = new ArrayList<Commit>();
+    private static final File SOURCE_CODE_DIRECTORY = new File("src");
 
     public void testSetsTheLatestUpdateToTheMostRecentCommitTheFirstTimeTheBuildIsRun() throws RepositoryException, IOException {
         GitClient gitClient = gitClient();
@@ -32,35 +35,69 @@ public class GitClientTest extends MockObjectTestCase {
             one(gitLogCommand).getLastRevisionChecked(); will(returnValue(LAST_REVISION_CHECKED));
         }});
 
-        String latestUpdate = gitClient.getLatestUpdate(buildLogger, REPOSITORY_URL, "plankey", NO_PREVIOUS_LATEST_UPDATE_TIME, new ArrayList<Commit>(), new File("src"));
+        String latestUpdate = gitClient.getLatestUpdate(buildLogger, REPOSITORY_URL, "plankey", NO_PREVIOUS_LATEST_UPDATE_TIME, commits, SOURCE_CODE_DIRECTORY);
 
         assertEquals(LAST_REVISION_CHECKED, latestUpdate);
+        assertTrue(commits.isEmpty());
+    }
 
-        /*
+    public void testDoesNotReturnAnyCommitsIfThereHaveNotBeenAnyCommentsSinceTheLastCheck() throws RepositoryException, IOException {
+        GitClient gitClient = gitClient();
 
-    public String getLatestUpdate(BuildLogger buildLogger, String repositoryUrl, String planKey, String lastRevisionChecked, List<Commit> commits, File sourceCodeDirectory) throws RepositoryException {
-        try {
-            pullCommand(sourceCodeDirectory).pullUpdatesFromRemoteRepository(buildLogger, repositoryUrl);
+        checking(new Expectations() {{
+            one(gitPullCommand).pullUpdatesFromRemoteRepository(buildLogger, REPOSITORY_URL);
+            one(gitLogCommand).extractCommits(); will(returnValue(new ArrayList<Commit>()));
+            one(gitLogCommand).getLastRevisionChecked(); will(returnValue(LAST_REVISION_CHECKED));
+        }});
 
-            ExtractorGitLogCommand gitLogCommand = logCommand(sourceCodeDirectory, lastRevisionChecked);
-            List<Commit> gitCommits = gitLogCommand.extractCommits();
-            String latestRevisionOnServer = gitLogCommand.getLastRevisionChecked();
-            if (lastRevisionChecked == null) {
-                log.info("Never checked logs for '" + planKey + "' on path '" + repositoryUrl + "'  setting latest revision to " + latestRevisionOnServer);
-                return latestRevisionOnServer;
-            }
-            if (!latestRevisionOnServer.equals(lastRevisionChecked)) {
-                log.info("Collecting changes for '" + planKey + "' on path '" + repositoryUrl + "' since " + lastRevisionChecked);
-                commits.addAll(gitCommits);
-            }
+        String latestUpdate = gitClient.getLatestUpdate(buildLogger, REPOSITORY_URL, "plankey", LAST_REVISION_CHECKED, commits, SOURCE_CODE_DIRECTORY);
 
+        assertEquals(LAST_REVISION_CHECKED, latestUpdate);
+        assertTrue(commits.isEmpty());
+    }
+
+
+    public void testReturnsTheNewCommitsAndLatestCommitTimeIfThereAreNewCommentsSinceTheLastCheck() throws RepositoryException, IOException {
+        GitClient gitClient = gitClient();
+        final ArrayList<Commit> latestCommits = new ArrayList<Commit>();
+        latestCommits.add(new CommitImpl());
+        latestCommits.add(new CommitImpl());
+
+        checking(new Expectations() {{
+            one(gitPullCommand).pullUpdatesFromRemoteRepository(buildLogger, REPOSITORY_URL);
+            one(gitLogCommand).extractCommits(); will(returnValue(latestCommits));
+            one(gitLogCommand).getLastRevisionChecked(); will(returnValue("2009-03-25 01:09:25 +0000"));
+        }});
+
+        String latestUpdate = gitClient.getLatestUpdate(buildLogger, REPOSITORY_URL, "plankey", LAST_REVISION_CHECKED, commits, SOURCE_CODE_DIRECTORY);
+
+        assertEquals("2009-03-25 01:09:25 +0000", latestUpdate);
+        assertEquals(2, commits.size());
+    }
+    /*
+
+public String getLatestUpdate(BuildLogger buildLogger, String repositoryUrl, String planKey, String lastRevisionChecked, List<Commit> commits, File sourceCodeDirectory) throws RepositoryException {
+    try {
+        pullCommand(sourceCodeDirectory).pullUpdatesFromRemoteRepository(buildLogger, repositoryUrl);
+
+        ExtractorGitLogCommand gitLogCommand = logCommand(sourceCodeDirectory, lastRevisionChecked);
+        List<Commit> gitCommits = gitLogCommand.extractCommits();
+        String latestRevisionOnServer = gitLogCommand.getLastRevisionChecked();
+        if (lastRevisionChecked == null) {
+            log.info("Never checked logs for '" + planKey + "' on path '" + repositoryUrl + "'  setting latest revision to " + latestRevisionOnServer);
             return latestRevisionOnServer;
-        } catch (IOException e) {
-            throw new RepositoryException("Failed to get latest update", e);
         }
+        if (!latestRevisionOnServer.equals(lastRevisionChecked)) {
+            log.info("Collecting changes for '" + planKey + "' on path '" + repositoryUrl + "' since " + lastRevisionChecked);
+            commits.addAll(gitCommits);
+        }
+
+        return latestRevisionOnServer;
+    } catch (IOException e) {
+        throw new RepositoryException("Failed to get latest update", e);
     }
+}
 */
-    }
 
     private GitClient gitClient() {
         return new GitClient(GIT_EXE) {
