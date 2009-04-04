780b5cc727f303f7dd475f5847c2a9caf162855a
Andy Pols <andy@pols.co.uk>
2009-04-04 13:53:41 +0100

    Extracted out the init command so I can test the client properly

---
+++ src/main/java/uk/co/pols/bamboo/gitplugin/commands/ExecutorGitInitCommand.java
@@ -0,0 +1,27 @@
+package uk.co.pols.bamboo.gitplugin.commands;
+
+import com.atlassian.bamboo.build.logger.BuildLogger;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ExecutorGitInitCommand implements GitInitCommand {
+    private static final Log log = LogFactory.getLog(ExecutorGitInitCommand.class);
+
+    private String gitExe;
+    private File sourceCodeDirectory;
+    private CommandExecutor commandExecutor;
+
+    public ExecutorGitInitCommand(String gitExe, File sourceCodeDirectory, CommandExecutor commandExecutor) {
+        this.gitExe = gitExe;
+        this.sourceCodeDirectory = sourceCodeDirectory;
+        this.commandExecutor = commandExecutor;
+    }
+
+    public void init(BuildLogger buildLogger) throws IOException {
+        log.info(buildLogger.addBuildLogEntry("Running '" + gitExe + " init'"));
+        commandExecutor.execute(new String[]{gitExe, "init"}, sourceCodeDirectory);
+    }
+}
---
+++ src/main/java/uk/co/pols/bamboo/gitplugin/commands/GitInitCommand.java
@@ -0,0 +1,9 @@
+package uk.co.pols.bamboo.gitplugin.commands;
+
+import com.atlassian.bamboo.build.logger.BuildLogger;
+
+import java.io.IOException;
+
+public interface GitInitCommand {
+    void init(BuildLogger buildLogger) throws IOException;
+}
