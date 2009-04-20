2cea99974ba03ce5e834f395dba6464d68052f98
Andy Pols <andy@pols.co.uk>
2009-04-20 02:32:31 +0100

    spike that authenticates and checksout the code

--- src/main/java/uk/co/pols/bamboo/gitplugin/authentication/SshSession.java
+++ src/main/java/uk/co/pols/bamboo/gitplugin/authentication/SshSession.java
@@ -1,9 +1,17 @@
 package uk.co.pols.bamboo.gitplugin.authentication;
 
 import com.jcraft.jsch.*;
+import com.atlassian.bamboo.repository.RepositoryException;
+import com.atlassian.bamboo.build.logger.BuildLogger;
+import com.atlassian.bamboo.build.LogEntry;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+
+import uk.co.pols.bamboo.gitplugin.GitClient;
+import uk.co.pols.bamboo.gitplugin.CmdLineGitClient;
+import uk.co.pols.bamboo.gitplugin.GitRepositoryConfig;
 
 public class SshSession {
     private static final String GIT_HUB_HOST = "github.com";
@@ -74,10 +82,64 @@ public class SshSession {
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, RepositoryException {
+
+
         SshSession sshSession = new SshSession();
         Session session = sshSession.getSession(new File("/Users/andy/.ssh/bamboo-gbp"), "bamboo-gbp");
 
+        System.out.println("session.isConnected() = " + session.isConnected());
+
+        GitClient client = new CmdLineGitClient();
+        GitRepositoryConfig gitRepositoryConfig = new GitRepositoryConfig();
+        gitRepositoryConfig.setRepositoryUrl("git://github.com/andypols/git-bamboo-plugin.git");
+        client.initialiseRepository(new File("/Users/andy/projects/moocow"), "KEY", null, gitRepositoryConfig, true, new BuildLogger() {
+            public List<LogEntry> getBuildLog() {
+                return null;
+            }
+
+            public List<LogEntry> getErrorLog() {
+                return null;
+            }
+
+            public List<String> getStringErrorLogs() {
+                return null;
+            }
+
+            public String addBuildLogEntry(LogEntry logEntry) {
+                return null;
+            }
+
+            public String addBuildLogEntry(String string) {
+                return null;
+            }
+
+            public String addBuildLogHeader(String string, boolean b) {
+                return null;
+            }
+
+            public String addErrorLogEntry(LogEntry logEntry) {
+                return null;
+            }
+
+            public String addErrorLogEntry(String string) {
+                return null;
+            }
+
+            public void startStreamingBuildLogs(int i, String string) {
+            }
+
+            public void stopStreamingBuildLogs() {
+            }
+
+            public void clearBuildLog() {
+            }
+
+            public long getTimeOfLastLog() {
+                return 0;
+            }
+        });
+//        System.out.println("Now lets try and do something with a git command!");
         session.disconnect();
     }
 }
