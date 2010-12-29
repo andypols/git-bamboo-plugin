47a780f8c98adfdc52fb9c24964afe70275e1b50
Andy <andy@Andys-MacBook-Pro.local>
2010-12-29 13:50:19 +0000

    Extend the Default WeRepository Viewer so we get all the jira links and good stuff

--- src/main/java/uk/co/pols/bamboo/gitplugin/GitHubWebRepositoryViewer.java
+++ src/main/java/uk/co/pols/bamboo/gitplugin/GitHubWebRepositoryViewer.java
@@ -5,59 +5,74 @@ import com.atlassian.bamboo.commit.CommitFile;
 import com.atlassian.bamboo.repository.Repository;
 import com.atlassian.bamboo.resultsummary.ResultsSummary;
 import com.atlassian.bamboo.utils.error.ErrorCollection;
-import com.atlassian.bamboo.webrepository.AbstractWebRepositoryViewer;
 import com.atlassian.bamboo.webrepository.CommitUrlProvider;
+import com.atlassian.bamboo.webrepository.DefaultWebRepositoryViewer;
 import com.atlassian.bamboo.ww2.actions.build.admin.create.BuildConfiguration;
 import org.apache.commons.configuration.HierarchicalConfiguration;
+import org.apache.log4j.Logger;
 
 import java.util.Arrays;
 import java.util.Collection;
 
 import static uk.co.pols.bamboo.gitplugin.GitRepositoryConfig.AvailableConfig.WEB_REPOSITORY;
 
-public class GitHubWebRepositoryViewer extends AbstractWebRepositoryViewer implements CommitUrlProvider {
+public class GitHubWebRepositoryViewer extends DefaultWebRepositoryViewer implements CommitUrlProvider {
+    private static Logger log = Logger.getLogger(GitHubWebRepositoryViewer.class);
     private GitRepositoryConfig gitRepositoryConfig = gitRepositoryConfig();
 
     @Override
     public Collection<String> getSupportedRepositories() {
+        log.info("********************* GitHubWebRepositoryViewer.getSupportedRepositories");
         return Arrays.asList("uk.co.pols.bamboo.gitplugin:github");
     }
 
     @Override
     public void populateFromConfig(HierarchicalConfiguration config) {
+        log.info("********************* GitHubWebRepositoryViewer.populateFromConfig");
         super.populateFromConfig(config);
         gitRepositoryConfig.populateFromConfig(config, WEB_REPOSITORY);
     }
 
     @Override
     public HierarchicalConfiguration toConfiguration() {
+        log.info("********************  GitHubWebRepositoryViewer.toConfiguration");
         return gitRepositoryConfig.toConfiguration(super.toConfiguration(), WEB_REPOSITORY);
     }
 
     @Override
     public ErrorCollection validate(BuildConfiguration buildConfiguration) {
+        log.info("********************  GitHubWebRepositoryViewer.validate");
         return gitRepositoryConfig.validate(super.validate(buildConfiguration), buildConfiguration, WEB_REPOSITORY);
     }
 
     public String getHtmlForCommitsFull(ResultsSummary resultsSummary, Repository repository) {
-        System.out.println("resultsSummary = " + resultsSummary);
-        return "<p>getHtmlForCommitsFull</p>";
+        log.info("****************  GitHubWebRepositoryViewer.getHtmlForCommitsFull");
+        String html = super.getHtmlForCommitsFull(resultsSummary,repository);
+        log.info("************* resultsSummary = " + html);
+
+        return html;
     }
 
     public String getHtmlForCommitsSummary(ResultsSummary resultsSummary, Repository repository) {
-        System.out.println("resultsSummary = " + resultsSummary);
-        return "<p>getHtmlForCommitsSummary</p>";
+        log.info("****************** GitHubWebRepositoryViewer.getHtmlForCommitsSummary");
+        String html = super.getHtmlForCommitsSummary(resultsSummary, repository);
+        log.info("resultsSummary = " + html);
+        return html;
     }
 
     public void setWebRepositoryUrl(String url) {
+        log.info("*****************  GitHubWebRepositoryViewer.setWebRepositoryUrl");
         gitRepositoryConfig.setWebRepositoryUrl(url);
     }
 
     public String getWebRepositoryUrl() {
+        log.info("****************  GitHubWebRepositoryViewer.getWebRepositoryUrl");
         return gitRepositoryConfig.getWebRepositoryUrl();
     }
 
     public String getWebRepositoryUrlForFile(CommitFile commitFile) {
+        log.info("**************** GitHubWebRepositoryViewer.getWebRepositoryUrlForFile");
+
         return gitRepositoryConfig.getWebRepositoryUrlForFile(commitFile);
     }
 
@@ -65,6 +80,7 @@ public class GitHubWebRepositoryViewer extends AbstractWebRepositoryViewer imple
         Rendering links in emails too
     */
     public String getWebRepositoryUrlForCommit(Commit commit, Repository repository) {
+        log.info("************** GitHubWebRepositoryViewer.getWebRepositoryUrlForCommit");
         return gitRepositoryConfig.getWebRepositoryUrlForCommit(commit);
     }
 
